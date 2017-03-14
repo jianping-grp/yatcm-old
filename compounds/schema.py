0ee0a8976a69fc28e1b649635627cdb5b51dcb28
zhonghua <zhonghua.wang@outlook.com>
2017-03-14 15:39:47 +0800

    add graphene

--- compounds/schema.py
+++ compounds/schema.py
@@ -17,7 +17,42 @@ class CompoundNode(DjangoObjectType):
             'chinese_name',
         ]
 
+    @classmethod
+    def get_node(cls, id, context, info):
+        try:
+            cpd = cls._meta.model.objects.get(id=id)
+        except cls._meta.model.DoesNotExist:
+            return None
+        return cpd
+
+
+class HerbNode(DjangoObjectType):
+    class Meta:
+        model = models.Herb
+        interfaces = (Node,)
+
+
+class PrescriptionNode(DjangoObjectType):
+    class Meta:
+        model = models.Prescription
+        interfaces = (Node,)
+
+
+class KEGGPathwayNode(DjangoObjectType):
+    class Meta:
+        model = models.KEGGPathway
+        interfaces = (Node,)
+
 
 class Query(AbstractType):
     compound = Node.Field(CompoundNode)
     all_compound = DjangoFilterConnectionField(CompoundNode)
+
+    herb = Node.Field(HerbNode)
+    all_herb = DjangoFilterConnectionField(HerbNode)
+
+    prescription = Node.Field(PrescriptionNode)
+    all_prescription = DjangoFilterConnectionField(PrescriptionNode)
+
+    keggpathway = Node.Field(KEGGPathwayNode)
+    all_keggpathway = DjangoFilterConnectionField(KEGGPathwayNode)
